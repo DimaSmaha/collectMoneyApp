04f8f8acd8d892db03e246b5254386605dd19d65
DimaSmaha <dmytrosmaha@gmail.com>
2023-07-24 18:37:44 +0300

    add new achievement

--- public/script.js
+++ public/script.js
@@ -312,6 +312,7 @@ function acceptEditGoal() {
 }
 
 let achievementOneComplete = false;
+let achievementTwoComplete = false;
 function checkAchievements() {
   //Achievement One, the user made 777 transaction
   if (
@@ -321,6 +322,15 @@ function checkAchievements() {
     const achievementOne = document.getElementById("achievement_1");
     achievementOne.style.display = "block";
     achievementOneComplete = true;
+  }
+  //Achievement Two, the user made a transaction on the Christmas 12-25
+  if (
+    transactionsArray[transactionsArray.length - 1].date.includes("-12-25T") &&
+    achievementTwoComplete == false
+  ) {
+    const achievementTwo = document.getElementById("achievement_2");
+    achievementTwo.style.display = "block";
+    achievementTwoComplete = true;
   }
 }
 
