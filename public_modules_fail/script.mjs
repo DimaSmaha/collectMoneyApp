2ccd6eb22b03fbc2c019a05e17a361e462341e8d
DimaSmaha <dmytrosmaha@gmail.com>
2023-08-12 23:38:12 +0300

    fix script.js

--- public_modules_fail/script.mjs
+++ public_modules_fail/script.mjs
@@ -4,6 +4,7 @@ import {
 } from "./modules/sloikGeneration.mjs";
 import { showPopup, hidePopup } from "./modules/popup.mjs";
 import { getData } from "./modules/insideSloik.mjs";
+
 window.onload = function () {
   if (document.title == "SloikApp Home") {
     renderExistingSloiks();
@@ -12,12 +13,17 @@ window.onload = function () {
     getData();
   }
 };
+
 const addSloikBtn = document.getElementById("add_sloik");
 if (document.title == "SloikApp Home") {
   addSloikBtn.onclick = function () {
     showPopup();
   };
 }
+
+let totalMoney = 0;
+let isGoalReached = false;
+let transactionsArray = [];
 const submitPopupBtn = document.getElementById("submitBtn");
 submitPopupBtn.onclick = function () {
   if (document.title == "SloikApp Home") {
