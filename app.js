1ed18a0d665f94cdb667033542eaef17b6c23059
DimaSmaha <dmytrosmaha@gmail.com>
2023-08-20 12:33:25 +0300

    upd server

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 5050;
 
-app.use(express.static("public"));
+app.use(express.static("public_modules"));
 
-app.listen(3000, () => {
-  console.log("listening port 3000");
+app.get("/home.html", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
