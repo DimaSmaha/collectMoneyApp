5451e549232481a7e5977a2553e540d433037430
DimaSmaha <dmytrosmaha@gmail.com>
2023-08-25 20:48:54 +0300

    add 3rd achievement

--- public_modules/modules/achievements.mjs
+++ public_modules/modules/achievements.mjs
@@ -1,10 +1,11 @@
 let achievement1Complete = false;
 let achievement2Complete = false;
-let achievementCounter = 2;
+let achievementCounter = 3;
 
 function checkAchievements(transactionsArray) {
   achievementOne(transactionsArray);
   achievementTwo(transactionsArray);
+  achievementThree(transactionsArray);
 }
 
 function achievementOne(transactionsArray) {
@@ -53,6 +54,41 @@ function achievementTwo(transactionsArray) {
       Cookies.set(achievementCookieName, JSON.stringify(true), {
         expires: 365,
       });
+    }
+  }
+}
+
+function achievementThree(transactionsArray) {
+  //Achievement Three, the user made a transaction on the Christmas 12-25
+  let achievementCookieName = "achievement_3_Complete";
+  if (Cookies.get(achievementCookieName) == undefined) {
+    Cookies.set(achievementCookieName, JSON.stringify(false), {
+      expires: 365,
+    });
+  }
+  if (transactionsArray != 0) {
+    if (transactionsArray.length >= 3) {
+      let transaction3 = transactionsArray[transactionsArray.length - 1].date;
+      let transaction1 = transactionsArray[transactionsArray.length - 3].date;
+
+      let transaction3date = new Date(transaction3).getDate();
+      let transaction3month = new Date(transaction3).getMonth();
+      let transaction3year = new Date(transaction3).getFullYear();
+      let transaction1date = new Date(transaction3).getDate();
+      let transaction1month = new Date(transaction3).getMonth();
+      let transaction1year = new Date(transaction3).getFullYear();
+      if (
+        transaction3date == transaction1date &&
+        transaction3month == transaction1month &&
+        transaction3year == transaction1year &&
+        JSON.parse(Cookies.get(achievementCookieName)) == false
+      ) {
+        const achievementThree = document.getElementById("achievement_3");
+        achievementThree.style.display = "block";
+        Cookies.set(achievementCookieName, JSON.stringify(true), {
+          expires: 365,
+        });
+      }
     }
   }
 }
