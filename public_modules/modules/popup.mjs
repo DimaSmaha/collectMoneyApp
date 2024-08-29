575d8fc173d957b0ee20e52be9548f4f3cc02452
DimaSmaha <dmytrosmaha@gmail.com>
2024-08-29 19:41:39 +0300

    delete values from input if creational popup closed

--- public_modules/modules/popup.mjs
+++ public_modules/modules/popup.mjs
@@ -1,6 +1,18 @@
 function showPopup() {
   const popup = document.getElementById("popupContainer");
   popup.style.display = "block";
+
+  titleInput.onclick = () => {
+    inputError1.style.display = "none";
+  };
+
+  descriptionInput.onclick = () => {
+    inputError2.style.display = "none";
+  };
+
+  goalSumInput.onclick = () => {
+    inputError3.style.display = "none";
+  };
 }
 
 const titleInput = document.getElementById("inputField1");
@@ -13,19 +25,12 @@ const inputError3 = document.getElementById("inputField3Error");
 function hidePopup() {
   const popup = document.getElementById("popupContainer");
   popup.style.display = "none";
+  titleInput.value = "";
+  descriptionInput.value = "";
+  goalSumInput.value = "";
   inputError1.style.display = "none";
   inputError2.style.display = "none";
   inputError3.style.display = "none";
 }
-
-titleInput.onclick = () => {
-  inputError1.style.display = "none";
-};
-descriptionInput.onclick = () => {
-  inputError2.style.display = "none";
-};
-goalSumInput.onclick = () => {
-  inputError3.style.display = "none";
-};
 
 export { showPopup, hidePopup };
