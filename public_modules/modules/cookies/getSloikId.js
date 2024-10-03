4092c32472ce64622729c961030046623ec8df8c
DimaSmaha <dmytrosmaha@gmail.com>
2024-10-03 15:59:55 +0300

    com

--- public_modules/modules/cookies/getSloikId.js
+++ public_modules/modules/cookies/getSloikId.js
@@ -1,3 +1,5 @@
+//** We should have this separated function because of mime issue in browser */
+
 function getSloikID(sloikNumber) {
   sloikID = sloikNumber;
   localStorage.setItem("currentSloikID", JSON.stringify(sloikID));
